fb1c18a0988823420a0c52c20ba366359b20aa9c
ouyang2007 <51415526+ouyang2007@users.noreply.github.com>
2024-08-30 01:18:06 +0000

    openai 0.28

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ openai.api_key  = david_openai_passwd
 ######################################################################
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
+
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
